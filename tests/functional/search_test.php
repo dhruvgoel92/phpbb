90e1af5bf4df156f2a58187869d1c9b6c90356f9
Dhruv <dhruv.goel92@gmail.com>
2012-08-19 11:32:06 +0530

    bla bla dont push

--- tests/functional/search_test.php
+++ tests/functional/search_test.php
@@ -14,7 +14,21 @@ class phpbb_functional_search_test extends phpbb_functional_test_case
 {
 	public function test_search()
 	{
-		$crawler = $this->request('GET', 'search.php?keywords=phpbb3');
-		$this->assertGreaterThan(0, $crawler->filter('.postbody')->count());
+		$this->login();
+		//$crawler = $this->request('GET', 'search.php?keywords=phpbb3');
+		$crawler = $this->request('GET', 'adm/index.php?sid=' . $this->sid . '&i=acp_search&mode=index');
+		$this->assertContains('Everything', $crawler->text());
+		//$this->assertGreaterThan(0, $crawler->filter('.postbody')->count());
 	}
+
+	public function test_search_form()
+	{
+		$crawler = $this->request('GET', 'search.php');
+		$this->assertContains($this->lang('SEARCH_KEYWORDS'), $crawler);
+
+		$form = $crawler->selectButton($this->lang('SEARCH'))->form();
+		$search = $this->client->submit($form, array('keywords' => 'phpbb3'));
+		$this->assertContains('Everything', $search->filter('.postbody')->text());
+	}
+
 }
